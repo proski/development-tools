af45f29ced3b448798fe257f40e3756124262778
Pavel Roskin <plroskin@gmail.com>
2026-02-23 15:55:26 -0800

    Remove obsolete scripts

--- checker/checker.py
+++
@@ -1,7 +0,0 @@
-'''Quickly check python files for broken syntax'''
-
-import sys
-
-for filename in sys.argv[1:]:
-    with open(filename, 'r') as fd:
-        compile(fd.read(), filename, 'exec')
--- usercustomize/usercustomize.py
+++
@@ -1,7 +0,0 @@
-""" Put site.USER_SITE in front of sys.path """
-
-import sys
-import site
-
-sys.path.remove(site.USER_SITE)
-sys.path.insert(0, site.USER_SITE)
